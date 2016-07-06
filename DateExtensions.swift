91e29973ce59663f7258733d83f1dd0a07de4f4b
Nix, Robert P <Nix.Robert@mayo.edu>
2016-07-06 08:20:09 -0500

    Initial commit.

---
+++ DateExtensions.swift
@@ -0,0 +1,165 @@
+//
+//  DateExtensions.swift
+//
+//  Created by Robert Nix on 6/18/16.
+//  Copyright © 2016 Nix, Robert P. All rights reserved.
+//
+
+import Foundation
+
+// Define methods that allow us to add and subtract
+// NSDateComponents instances
+
+// The addition and subtraction code is nearly the same,
+// so we've factored it out into this method
+func combineComponents(lhs: NSDateComponents,
+                       rhs: NSDateComponents,
+                       _ multiplier: Int = 1)
+    -> NSDateComponents
+{
+    let result = NSDateComponents()
+    let undefined = Int(NSDateComponentUndefined)
+    
+    result.second = ((lhs.second != undefined ? lhs.second : 0) +
+        (rhs.second != undefined ? rhs.second : 0) * multiplier)
+    result.minute = ((lhs.minute != undefined ? lhs.minute : 0) +
+        (rhs.minute != undefined ? rhs.minute : 0) * multiplier)
+    result.hour = ((lhs.hour != undefined ? lhs.hour : 0) +
+        (rhs.hour != undefined ? rhs.hour : 0) * multiplier)
+    result.day = ((lhs.day != undefined ? lhs.day : 0) +
+        (rhs.day != undefined ? rhs.day : 0) * multiplier)
+    result.month = ((lhs.month != undefined ? lhs.month : 0) +
+        (rhs.month != undefined ? rhs.month : 0) * multiplier)
+    result.year = ((lhs.year != undefined ? lhs.year : 0) +
+        (rhs.year != undefined ? rhs.year : 0) * multiplier)
+    return result
+}
+
+// With combineComponents defined,
+// overloading + and - is simple
+
+func +(lhs: NSDateComponents, rhs: NSDateComponents) -> NSDateComponents
+{
+    return combineComponents(lhs, rhs: rhs)
+}
+
+func -(lhs: NSDateComponents, rhs: NSDateComponents) -> NSDateComponents
+{
+    return combineComponents(lhs, rhs: rhs, -1)
+}
+
+// We'll need to overload unary - so we can negate components
+prefix func -(components: NSDateComponents) -> NSDateComponents {
+    let result = NSDateComponents()
+    let undefined = Int(NSDateComponentUndefined)
+    
+    if(components.second != undefined) { result.second = -components.second }
+    if(components.minute != undefined) { result.minute = -components.minute }
+    if(components.hour != undefined) { result.hour = -components.hour }
+    if(components.day != undefined) { result.day = -components.day }
+    if(components.month != undefined) { result.month = -components.month }
+    if(components.year != undefined) { result.year = -components.year }
+    return result
+}
+
+// Let's make it easy to add dates and components,
+// and subtract components from dates
+
+// Date + component
+func +(lhs: NSDate, rhs: NSDateComponents) -> NSDate
+{
+    return NSCalendar.currentCalendar().dateByAddingComponents(rhs,
+                                                               toDate: lhs,
+                                                               options: [])!
+}
+
+// Component + date
+func +(lhs: NSDateComponents, rhs: NSDate) -> NSDate
+{
+    return rhs + lhs
+}
+
+// Date - component
+// (Component - date doesn't make sense)
+func -(lhs: NSDate, rhs: NSDateComponents) -> NSDate
+{
+    return lhs + (-rhs)
+}
+
+
+// Next, we extend Int to bring some Ruby-like magic
+// to date components
+
+extension Int {
+    
+    var seconds: NSDateComponents {
+        let components = NSDateComponents()
+        components.second = self;
+        return components
+    }
+    
+    var second: NSDateComponents {
+        return self.seconds
+    }
+    
+    var minutes: NSDateComponents {
+        let components = NSDateComponents()
+        components.minute = self;
+        return components
+    }
+    
+    var minute: NSDateComponents {
+        return self.minutes
+    }
+    
+    var hours: NSDateComponents {
+        let components = NSDateComponents()
+        components.hour = self;
+        return components
+    }
+    
+    var hour: NSDateComponents {
+        return self.hours
+    }
+    
+    var days: NSDateComponents {
+        let components = NSDateComponents()
+        components.day = self;
+        return components
+    }
+    
+    var day: NSDateComponents {
+        return self.days
+    }
+    
+    var weeks: NSDateComponents {
+        let components = NSDateComponents()
+        components.day = 7 * self;
+        return components
+    }
+    
+    var week: NSDateComponents {
+        return self.weeks
+    }
+    
+    var months: NSDateComponents {
+        let components = NSDateComponents()
+        components.month = self;
+        return components
+    }
+    
+    var month: NSDateComponents {
+        return self.months
+    }
+    
+    var years: NSDateComponents {
+        let components = NSDateComponents()
+        components.year = self;
+        return components
+    }
+    
+    var year: NSDateComponents {
+        return self.years
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+DateExtensions
+
+This takes some of the complication out of date math. Using
+this extension allows you to code in a more natural way,
+such as things like 
+
+var newDate = NSDate() + 6.days
